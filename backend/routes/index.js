a74abaa43174d626cbff447357aed0aeb8fb4e38
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-10-20 15:57:43 +0530

    backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var users=[
+  {
+  email:'abc@gmail.com', password:'password'
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("welcome to backend server");
+  
 });
+
+router.post('/',function(req,res){
+  let result=user.find(user=>user.email==req.body.email);
+  if(result){
+    
+      if(result.password==req.body.password){
+        res.status(200).send({
+          message:"Successful login"
+        })
+      }
+    
+  }
+});
+
 
 module.exports = router;
